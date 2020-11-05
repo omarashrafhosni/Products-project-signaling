129777847e375b7ffb78092d2a9f41930678c13f
Omar <omarashrafhosni@gmail.com>
2020-11-05 02:49:25 +0200

    Product Signaling project

---
+++ app/src/main/java/app/AppConfig.java
@@ -0,0 +1,10 @@
+package app;
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://172.20.10.9/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://171.20.10.9/android_login_api/register.php";
+
+}
